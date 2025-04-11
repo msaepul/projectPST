3cc81b76a143532d225a31bae94a734a7521f1d7
ALFI <99178972+ALFI-S0@users.noreply.github.com>
2025-04-11 14:08:00 +0700

    Update maskapai.php

--- app/Models/maskapai.php
+++ app/Models/maskapai.php
@@ -1,17 +1,1 @@
-<?php
 
-namespace App\Models;
-
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
-
-class Maskapai extends Model
-{
-    use HasFactory;
-
-    protected $fillable = [
-        'id',
-        'kode_maskapai',
-        'nama_maskapai',
-    ];
-}
