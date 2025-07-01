d99a8a570c7555298d31092dccfbfee940857006
ALFI-S0 <kujojotaro158@gmail.com>
2025-07-01 02:31:19 +0000

    ubuntubaru5

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -40,7 +40,26 @@
 <body class="hold-transition sidebar-mini layout-fixed">
 
     <div class="wrapper">
+        <nav class="main-header navbar navbar-expand navbar-white navbar-light">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    {{-- <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i
+                            class="fas fa-bars"></i></a> --}}
+                </li>
+            </ul>
 
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item dropdown">
+                    <a class="nav-link" data-toggle="dropdown" href="#">
+                        <i class="fa fa-caret-down"></i>
+                    </a>
+                    <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
+                        <a href="{{ route('data_diri.biodata') }}" class="dropdown-item">Show Profile</a>
+                        <a href="{{ url('logout') }}" class="dropdown-item">Logout</a>
+                    </div>
+                </li>
+            </ul>
+        </nav>
         <!-- Sidebar -->
         <aside class="main-sidebar sidebar-dark-navy elevation-4">
             <div class="user-panel mt-3 pb-3 mb-3 d-flex align-items-center px-3">
--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,7 @@ use App\Http\Controllers\Data_diriController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('auth.login');
+    return view('/dashboard');
 })->name('home');
 
 Route::get('/dashboard', [HoController::class, 'dashboard'])->middleware(['auth', 'verified'])->name('dashboard');
