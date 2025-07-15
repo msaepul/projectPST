f0156466e3a39f0a43a7e6575a3bce5f82437d73
ALFI-S0 <kujojotaro158@gmail.com>
2025-07-15 08:33:08 +0000

    perbaikan menu

--- resources/views/partials/sidebar/bm.blade.php
+++ resources/views/partials/sidebar/bm.blade.php
@@ -4,38 +4,36 @@
                     request()->is('formpst/index_surat');
 @endphp
 
-<!-- === Menu Surat Tugas === -->
-<li class="nav-item has-treeview {{ $isSuratTugas ? 'menu-open' : '' }}">
-    <a href="#" class="nav-link {{ $isSuratTugas ? 'active' : '' }}">
-        <i class="nav-icon fas fa-envelope"></i>
-        <p>Surat Tugas<i class="fas fa-angle-left right"></i></p>
+<!-- === Section Title (opsional) === -->
+<li class="nav-header">SURAT TUGAS</li>
+
+<!-- Menu Surat Tugas -->
+<li class="nav-item">
+    <a href="{{ route('formpst.index_keluar') }}"
+       class="nav-link {{ request()->is('formpst/index_keluar') ? 'active' : '' }}">
+        <i class="nav-icon fas fa-envelope-open-text"></i>
+        <p>Surat Keluar</p>
     </a>
-    <ul class="nav nav-treeview">
-        <li class="nav-item">
-            <a href="{{ route('formpst.index_keluar') }}"
-               class="nav-link {{ request()->is('formpst/index_keluar') ? 'active' : '' }}">
-                <i class="far fa-circle nav-icon"></i>
-                <p>Surat Keluar</p>
-            </a>
-        </li>
-        <li class="nav-item">
-            <a href="{{ route('formpst.index_masuk') }}"
-               class="nav-link {{ request()->is('formpst/index_masuk') ? 'active' : '' }}">
-                <i class="far fa-circle nav-icon"></i>
-                <p>Surat Masuk</p>
-            </a>
-        </li>
-        <li class="nav-item">
-            <a href="{{ route('formpst.index_surat') }}"
-               class="nav-link {{ request()->is('formpst/index_surat') ? 'active' : '' }}">
-                <i class="far fa-circle nav-icon"></i>
-                <p>Surat Tugas</p>
-            </a>
-        </li>
-    </ul>
+</li>
+<li class="nav-item">
+    <a href="{{ route('formpst.index_masuk') }}"
+       class="nav-link {{ request()->is('formpst/index_masuk') ? 'active' : '' }}">
+        <i class="nav-icon fas fa-envelope"></i>
+        <p>Surat Masuk</p>
+    </a>
+</li>
+<li class="nav-item">
+    <a href="{{ route('formpst.index_surat') }}"
+       class="nav-link {{ request()->is('formpst/index_surat') ? 'active' : '' }}">
+        <i class="nav-icon fas fa-file-alt"></i>
+        <p>Surat Tugas</p>
+    </a>
 </li>
 
-<!-- === Menu List Keberangkatan === -->
+<!-- === Section Title (opsional) === -->
+<li class="nav-header">TIKET</li>
+
+<!-- Menu List Keberangkatan -->
 <li class="nav-item">
     <a href="{{ route('formpst.show_ticket') }}"
        class="nav-link {{ request()->is('formpst/show_ticket') ? 'active' : '' }}">
